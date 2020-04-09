b9d826bff8822196e2631a7c17e669ad9010a5fa
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-04-10 00:23:48 +0800

    Initial commit

---
+++ lib/dop.rb
@@ -0,0 +1,6 @@
+require "dop/version"
+
+module Dop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/dop_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Dop do
+  it "has a version number" do
+    expect(Dop::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
