13ed0883830443f9577c69e266dd70c7c3780eba
James Dela Cruz <delacruzjamesmartin@gmail.com>
2020-04-10 09:24:24 +0800

    Fixed generators file install

---
+++ lib/generators/dragonpay/install_generator.rb
@@ -0,0 +1,15 @@
+module Dragonpay
+  module Generators
+    class InstallGenerator < Rails::Generator::Base
+      source_root File.expand_path('../templates', __FILE__)
+
+      def copy_config_file
+        copy_file "dop.yml", "config/dop.yml"
+      end
+
+      def copy_initializer_file
+        copy_file "dop.rb",  "config/initializers/dop.rb"
+      end
+    end
+  end
+end
